43aaa71414a27a6463a871d644a468bcf8cb8f0a
Andy Smit <awsmit@shaw.ca>
2025-09-11 18:31:30 -0600

    Convert Tabs to space

--- app/src/main.c
+++ app/src/main.c
@@ -20,27 +20,27 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 int main(void)
 {
-	int ret;
-	bool led_state = true;
+    int ret;
+    bool led_state = true;
 
-	if (!gpio_is_ready_dt(&led)) {
-		return 0;
-	}
+    if (!gpio_is_ready_dt(&led)) {
+        return 0;
+    }
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		return 0;
-	}
+    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0) {
+        return 0;
+    }
 
-	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			return 0;
-		}
+    while (1) {
+        ret = gpio_pin_toggle_dt(&led);
+        if (ret < 0) {
+            return 0;
+        }
 
-		led_state = !led_state;
-		printf("LED state: %s\n", led_state ? "ON" : "OFF");
-		k_msleep(SLEEP_TIME_MS);
-	}
-	return 0;
+        led_state = !led_state;
+        printf("LED state: %s\n", led_state ? "ON" : "OFF");
+        k_msleep(SLEEP_TIME_MS);
+    }
+    return 0;
 }
