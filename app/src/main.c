bbea316c385c6cfa3375e20d811af1b020e06ded
ysamantar1 <52938483+yasmin537@users.noreply.github.com>
2025-10-02 20:37:47 -0600

    Add min main function & setup proj

---
+++ app/src/main.c
@@ -0,0 +1,15 @@
+
+#include <zephyr/kernal.h>
+#include <zephyr/drivers/gpio.h>
+
+// #define LED0_NODE DT_ALIAS(led0)
+
+// static const struct gpio_dt_spec led0 = GPIO_DT
+
+int main(void) {
+    while(1) {
+
+    }
+
+    return 0;
+}
