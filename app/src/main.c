3733a29ce5f90950ae85e97649dcbe8c2b190078
ysamantar1 <52938483+yasmin537@users.noreply.github.com>
2026-01-22 18:30:02 -0700

    note

--- app/src/main.c
+++ app/src/main.c
@@ -10,7 +10,31 @@
 #include "BTN.h"
 #include "LED.h"
 
+#include <errno.h>
+#include <inttypes.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/byteorder.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/types.h>
+
 #define SLEEP_MS 1
+
+static struct bt_uuid_128 BLE_CUSTOM_SERVICE_UUID =
+    BT_UUID_INIT_128(BT_UUID_128_ENCODE(0x11111111, 0x2222, 0x3333, 0x4444, 0x000000000001));
+static struct bt_uuid_128 BLE_CUSTOM_CHARACTERISTIC_UUID =
+    BT_UUID_INIT_128(BT_UUID_128_ENCODE(0x11111111, 0x2222, 0x3333, 0x4444, 0x000000000002));
+
+static struct bt_conn* my_connection;
+
+static void ble_on_device_connected(struct bt_conn* conn, uint8_t err);
+static void ble_on_device_disconnected(struct bt_conn* conn, uint8_t reason);
 
 int main(void) {
 
@@ -25,4 +49,12 @@ int main(void) {
     k_msleep(SLEEP_MS);
   }
 	return 0;
+}
+
+static void ble_on_device_connected(struct bt_conn* conn, uint8_t err) {
+  ...
+}
+
+static void ble_on_device_disconnected(struct bt_conn* conn, uint8_t reason) {
+  ...
 }
